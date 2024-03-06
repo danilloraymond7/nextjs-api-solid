8dfbd8eb3c11c1a37b67238f8710e232f7ca1dca
Danillo Raymund <33788284+danilloraymond7@users.noreply.github.com>
2024-03-06 15:12:55 -0300

    lint: formatacao

--- src/repositories/user/implementations/PrismaUserRepository.ts
+++ src/repositories/user/implementations/PrismaUserRepository.ts
@@ -1,51 +1,51 @@
-import prisma from '@/database';
-import {type User} from '@/entities/users/User';
-import {type IUserRepository} from '@/repositories/user/IUsuarioRepository';
+import prisma from '@/database'
+import { type User } from '@/entities/users/User'
+import { type IUserRepository } from '@/repositories/user/IUsuarioRepository'
 
 class PrismaUserRepository implements IUserRepository {
-	async getUserByEmail(email: string): Promise<User | undefined> {
-		const user = await prisma.user.findFirst({
-			where: {
-				email,
-			},
-		});
+  async getUserByEmail (email: string): Promise<User | null> {
+    const user = await prisma.user.findFirst({
+      where: {
+        email
+      }
+    })
 
-		if (!user) {
-			return undefined; // Retorna null se o usuário não for encontrado
-		}
+    if (user === null) {
+      return null
+    }
 
-		return user;
-	}
+    return user
+  }
 
-	async saveDataOauth(userData: User): Promise<boolean> {
-		const saved = await prisma.user.create({
-			data: {
-				...userData,
-			},
-		});
+  async saveDataOauth (userData: User): Promise<boolean> {
+    const saved = await prisma.user.create({
+      data: {
+        ...userData
+      }
+    })
 
-		if (!saved) {
-			return false;
-		}
+    if (saved === null) {
+      return false
+    }
 
-		return true;
-	}
+    return true
+  }
 
-	async updateDataOauth(userData: User): Promise<boolean> {
-		const updated = await prisma.user.updateMany({
-			data: {
-				...userData,
-			},
-			where: {
-				id: userData.id,
-			},
-		});
-		if (!updated) {
-			return false;
-		}
+  async updateDataOauth (userData: User): Promise<boolean> {
+    const updated = await prisma.user.updateMany({
+      data: {
+        ...userData
+      },
+      where: {
+        id: userData.id
+      }
+    })
+    if (updated === null) {
+      return false
+    }
 
-		return true;
-	}
+    return true
+  }
 }
 
-export {PrismaUserRepository};
+export { PrismaUserRepository }
